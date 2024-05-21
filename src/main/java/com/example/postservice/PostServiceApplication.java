c8f031c412420ab106e3f2f4eaa0dc61e95ebe3e
codrin2 <hssang127@gmail.com>
2024-05-21 10:16:33 +0900

    [fix] dev db 연결 수정

--- src/main/java/com/example/postservice/PostServiceApplication.java
+++ src/main/java/com/example/postservice/PostServiceApplication.java
@@ -1,11 +1,14 @@
 package com.example.postservice;
 
+import com.example.postservice.config.DatasourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties(DatasourceProperties.class)
 public class PostServiceApplication {
 
     public static void main(String[] args) {
