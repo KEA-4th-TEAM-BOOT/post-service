2700d9fc9bc1d18fa0391105b636e82b650714af
codrin2 <hssang127@gmail.com>
2024-05-21 18:04:14 +0900

    [fix] import 수정

--- src/main/java/com/example/postservice/PostServiceApplication.java
+++ src/main/java/com/example/postservice/PostServiceApplication.java
@@ -1,6 +1,6 @@
 package com.example.postservice;
 
-import com.example.postservice.config.DatasourceProperties;
+//import com.example.postservice.config.DatasourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
